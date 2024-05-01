344492432df282a356a696114aaf75f035861484
jennnywu <jingjw999@gmail.com>
2024-05-01 04:36:55 -0400

    ugh

--- script.js
+++ script.js
@@ -50,125 +50,67 @@ input.addEventListener('keypress', function(event) {
         const task = input.value.trim();
         if(task !== '') {
             const listItem = document.createElement('ul');
+            const trashIcon = document.createElement('i');
+
             listItem.innerHTML = `<input type="checkbox">${task}`;
             incompleteList.appendChild(listItem);
             input.value = '';
+            
+            trashIcon.className = 'fa-solid fa-trash';
+            trashIcon.classList.add('trash');
+            listItem.appendChild(trashIcon);
         }
     }
 });
 
-cupsPlayButton.addEventListener('click', () => {
-    if(cupsAudio.paused) {
-        cupsAudio.play();
-        cupsIcon.classList.remove('fa-play');
-        cupsIcon.classList.add('fa-pause');
-    } else {
-        cupsAudio.pause();
-        cupsIcon.classList.remove('fa-pause');
-        cupsIcon.classList.add('fa-play');
-    }
-})
-
-customersPlayButton.addEventListener('click', () => {
-    if(customersAudio.paused) {
-        customersAudio.play();
-        customersIcon.classList.remove('fa-play');
-        customersIcon.classList.add('fa-pause');
-    } else {
-        customersAudio.pause();
-        customersIcon.classList.remove('fa-pause');
-        customersIcon.classList.add('fa-play');
+document.addEventListener('click', function(event) {
+    if(event.target.classList.contains('trash')) {
+        event.target.parentElement.remove();
     }
 });
 
-drinksPlayButton.addEventListener('click', () => {
-    if(drinksAudio.paused) {
-        drinksAudio.play();
-        drinksIcon.classList.remove('fa-play');
-        drinksIcon.classList.add('fa-pause');
-    } else {
-        drinksAudio.pause();
-        drinksIcon.classList.remove('fa-pause');
-        drinksIcon.classList.add('fa-play');
-    }
-});
+drinksPlayButton.addEventListener('click', toggleAudio.bind(null, drinksAudio, drinksIcon));
+cupsPlayButton.addEventListener('click', toggleAudio.bind(null, cupsAudio, cupsIcon));
+customersPlayButton.addEventListener('click', toggleAudio.bind(null, customersAudio, customersIcon));
+coffeePlayButton.addEventListener('click', toggleAudio.bind(null, coffeeAudio, coffeeIcon));
+fireplacePlayButton.addEventListener('click', toggleAudio.bind(null, fireplaceAudio, fireplaceIcon));
+rainPlayButton.addEventListener('click', toggleAudio.bind(null, rainAudio, rainIcon));
+sunPlayButton.addEventListener('click', toggleAudio.bind(null, sunAudio, sunIcon));
 
-coffeePlayButton.addEventListener('click', () => {
-    if(coffeeAudio.paused) {
-        coffeeAudio.play();
-        coffeeIcon.classList.remove('fa-play');
-        coffeeIcon.classList.add('fa-pause');
+function toggleAudio(audio, icon) {
+    if (audio.paused) {
+        audio.play();
+        icon.classList.remove('fa-play');
+        icon.classList.add('fa-pause');
     } else {
-        coffeeAudio.pause();
-        coffeeIcon.classList.remove('fa-pause');
-        coffeeIcon.classList.add('fa-play');
+        audio.pause();
+        icon.classList.remove('fa-pause');
+        icon.classList.add('fa-play');
     }
-});
-
-coffeePlayButton.addEventListener('click', () => {
-    if(coffeeMachineAudio.paused) {
-        coffeeMachineAudio.play();
-        coffeeIcon.classList.remove('fa-play');
-        coffeeIcon.classList.add('fa-pause');
-    } else {
-        coffeeMachineAudio.pause();
-        coffeeIcon.classList.remove('fa-pause');
-        coffeeIcon.classList.add('fa-play');
-    }
-});
-
-fireplacePlayButton.addEventListener('click', () => {
-    if(fireplaceAudio.paused) {
-        fireplaceAudio.play();
-        fireplaceIcon.classList.remove('fa-play');
-        fireplaceIcon.classList.add('fa-pause');
-    } else {
-        fireplaceAudio.pause();
-        fireplaceIcon.classList.remove('fa-pause');
-        fireplaceIcon.classList.add('fa-play');
-    }
-});
-
-rainPlayButton.addEventListener('click', () => {
-    if(rainAudio.paused) {
-        rainAudio.play();
-        rainIcon.classList.remove('fa-play');
-        rainIcon.classList.add('fa-pause');
-    } else {
-        rainAudio.pause();
-        rainIcon.classList.remove('fa-pause');
-        rainIcon.classList.add('fa-play');
-    }
-});
-
-sunPlayButton.addEventListener('click', () => {
-    if(sunAudio.paused) {
-        sunAudio.play();
-        sunIcon.classList.remove('fa-play');
-        sunIcon.classList.add('fa-pause');
-    } else {
-        sunAudio.pause();
-        sunIcon.classList.remove('fa-pause');
-        sunIcon.classList.add('fa-play');
-    }
-});
+}
 
 document.querySelectorAll('.slider').forEach(slider => {
-    const cupsAudioElement = document.getElementById('cups-audio');
-    const customersAudioElement = document.getElementById('customers-audio');
-    const drinksAudioElement = document.getElementById('drinks-audio');
-    const coffeeAudioElement = document.getElementById('coffee-audio');
-    const fireplaceAudioElement = document.getElementById('fireplace-audio');
-    const rainAudioElement = document.getElementById('rain-audio');
-    const sunAudioElement = document.getElementById('sun-audio');
-
     slider.addEventListener('input', () => {
-        cupsAudioElement.volume = slider.value / 100;
-        customersAudioElement.volume = slider.value / 100;
-        drinksAudioElement.volume = slider.value / 100;
-        coffeeAudioElement.volume = slider.value / 100;
-        fireplaceAudioElement.volume = slider.value / 100;
-        rainAudioElement.volume = slider.value / 100;
-        sunAudioElement.volume = slider.value / 100;
+        cupsAudio.volume = slider.value / 100;
+        customersAudio.volume = slider.value / 100;
+        drinksAudio.volume = slider.value / 100;
+        coffeeAudio.volume = slider.value / 100;
+        fireplaceAudio.volume = slider.value / 100;
+        rainAudio.volume = slider.value / 100;
+        sunAudio.volume = slider.value / 100;
+        
+        const volumeIcon = slider.parentElement.querySelector('.fa-volume-high, .fa-volume-low, .fa-volume-off');
+
+        console.log(slider.value);
+
+        if(volumeIcon) {
+            if(slider.value == 0 && slider.value == null) {
+                volumeIcon.className = 'fa-solid fa-volume-off';
+            } else if(slider.value <= 50) {
+                volumeIcon.className = 'fa-solid fa-volume-low';
+            } else {
+                volumeIcon.className = 'fa-solid fa-volume-high';
+            }
+        }
     });
 });
